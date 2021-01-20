496a65d8939c535421c10f298cbcef3554c095b0
Ryosuke Uchiyama <uchi63rs@yahoo.co.jp>
2021-01-20 22:45:05 +0900

    コメント強化

--- CreateWindowSample/Program.cpp
+++ CreateWindowSample/Program.cpp
@@ -10,12 +10,15 @@
 /// <returns></returns>
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
+	// ウィンドウメッセージによる分岐
 	switch (uMessage)
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
+	case WM_DESTROY:			// ウィンドウの破棄要求
+		// メッセージキューにWM_QUITメッセージをポストする
+		::PostQuitMessage(0);
 		break;
-	default:
+	default:					// デフォルト処理
+		// デフォルトのメッセージ処理に流す
 		return ::DefWindowProc(hWnd, uMessage, wParam, lParam);
 	}
 	return 0;
@@ -39,38 +42,38 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	// ウィンドウクラス設定
 	::ZeroMemory(&wcx, sizeof(wcx));
-	wcx.cbSize = sizeof(WNDCLASSEX);
-	wcx.style = CS_HREDRAW | CS_VREDRAW;
-	wcx.lpfnWndProc = WndProc;
-	wcx.cbClsExtra = 0;
-	wcx.cbWndExtra = 0;
-	wcx.hInstance = hInstance;
-	wcx.hIcon = ::LoadIcon(NULL, IDI_APPLICATION);
-	wcx.hIconSm = ::LoadIcon(NULL, IDI_WINLOGO);
-	wcx.hCursor = ::LoadCursor(NULL, IDC_ARROW);
-	wcx.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
-	wcx.lpszMenuName = NULL;
-	wcx.lpszClassName = szWindowClass;
+	wcx.cbSize = sizeof(WNDCLASSEX);								// 構造体のサイズ
+	wcx.style = CS_HREDRAW | CS_VREDRAW;							// ウィンドウスタイル
+	wcx.lpfnWndProc = WndProc;										// ウィンドウのメッセージを処理するコールバック関数
+	wcx.cbClsExtra = 0;												// ウィンドウクラス構造体の後ろに割り当てる補足バイト数
+	wcx.cbWndExtra = 0;												// ウィンドウインスタンスの後ろに割り当てる補足バイト数
+	wcx.hInstance = hInstance;										// このクラスのためのウィンドウプロシージャがあるインスタンスハンドル
+	wcx.hIcon = ::LoadIcon(NULL, IDI_APPLICATION);					// アイコンのハンドル
+	wcx.hIconSm = ::LoadIcon(NULL, IDI_WINLOGO);					// 小さいサイズのアイコンのハンドル
+	wcx.hCursor = ::LoadCursor(NULL, IDC_ARROW);					// マウスカーソルのハンドル
+	wcx.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);		// ウィンドウ背景色
+	wcx.lpszMenuName = NULL;										// デフォルトメニュー名
+	wcx.lpszClassName = szWindowClass;								// このウィンドウクラスにつける名前
 
 	// ウィンドウクラス登録エラー
-	if (!RegisterClassEx(&wcx))
+	if (!::RegisterClassEx(&wcx))
 	{
 		return 0;
 	}
 
 	// ウィンドウ生成
 	hWnd = ::CreateWindow(
-		szWindowClass,
-		szTitle,
-		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL);
+		szWindowClass,							// ウィンドウクラス名
+		szTitle,								// ウィンドウタイトル
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,		// ウィンドウスタイル
+		CW_USEDEFAULT,							// X座標
+		CW_USEDEFAULT,							// Y座標
+		CW_USEDEFAULT,							// 幅
+		CW_USEDEFAULT,							// 高さ
+		NULL,									// 親ウィンドウのハンドル
+		NULL,									// メニューハンドル
+		hInstance,								// ウィンドウインスタンス
+		NULL);									// WM_CREATEでLPARAMに渡したい値
 
 	// ウィンドウ生成エラー
 	if (!hWnd)
@@ -81,7 +84,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	// メッセージループ
 	while (::GetMessage(&msg, NULL, 0, 0) != 0)
 	{
+		// 仮想キーメッセージを文字メッセージに変換し、呼び出し側スレッドのメッセージキューにポストする
 		::TranslateMessage(&msg);
+		// ウィンドウプロシージャへメッセージをディスパッチする
 		::DispatchMessage(&msg);
 	}
 
